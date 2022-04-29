7f38da3fc5c3661b8e195479d8158bc875822a14
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2022-04-29 08:36:57 -0400

    v 0.0.2 ready to publish

--- example/index.tsx
+++ example/index.tsx
@@ -5,7 +5,7 @@ import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../.';
 
 const product = {
   id: '1',
-  title: 'Memezolanos Card Mug'
+  title: 'Memezolanos Card Mug v2'
 }
 
 const App = () => {
@@ -19,9 +19,9 @@ const App = () => {
     >
       {({ count, reset, increaseBy, isMaxCountReached, maxCount }) => (
         <>
-          <ProductImage/>
-          <ProductTitle/>
-          <ProductButtons/>
+          <ProductImage />
+          <ProductTitle />
+          <ProductButtons />
         </>
       )}
     </ProductCard>
